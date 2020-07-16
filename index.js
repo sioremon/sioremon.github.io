64b344e8ed2680685b7ed70aeb41b79746d264f6
sioremon <t17651sn@sfc.keio.ac.jp>
2020-07-16 18:59:38 +0900

    jsga

--- index.js
+++
@@ -1,9 +0,0 @@
-if (window.matchMedia('(max-width: 767px)').matches) {
-    //スマホ処理
-} else if (window.matchMedia('(min-width:768px)').matches) {
-    //PC処理
-}
-
-function openwindow(){
-  var win = window.open('http://www.yahoo.co.jp', '_blank');
-}
